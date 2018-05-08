25895058f39f94cf8c6f748121d30e96728944e4
Tiago Cordeiro <tiago@mulhergorila.com>
2018-05-08 20:15:26 -0300

    Fix: Adiciona rota summary

--- lme/app.py
+++ lme/app.py
@@ -342,6 +342,63 @@ def create_app():
                                    cotacao_semana_02, media_semana_02,
                                    cotacao_semana_01, media_semana_01])
 
+    @application.route('/summary', methods=['GET'])
+    def summary():
+
+        parse.uses_netloc.append("postgres")
+        url = parse.urlparse(os.environ["DATABASE_URL"])
+
+        conn = psycopg2.connect(
+            database=url.path[1:],
+            user=url.username,
+            password=url.password,
+            host=url.hostname,
+            port=url.port
+        )
+
+        query = """
+                SELECT * FROM cotacao_lme
+                WHERE cotacao_lme NOTNULL
+                ORDER BY "Date" DESC LIMIT 1
+                """
+
+        cotacaoatual = pd.read_sql(query, conn)
+
+        cotacaoatual.columns = ['Data', 'Cobre', 'Zinco', 'Aluminio', 'Chumbo',
+                                'Estanho', 'Niquel', 'Dolar']
+
+        df = pd.DataFrame(cotacaoatual)
+
+        df['Data'] = pd.to_datetime(df['Data'], utc=True)
+
+        df = df.set_index(df['Data'])
+
+        df = df.drop('Data', axis=1)
+
+        df.fillna(method='ffill', inplace=True)
+
+        cobre = list(df['Cobre'])
+        zinco = list(df['Zinco'])
+        aluminio = list(df['Aluminio'])
+        chumbo = list(df['Chumbo'])
+        estanho = list(df['Estanho'])
+        niquel = list(df['Niquel'])
+        dolar = list(df['Dolar'])
+        data = list(df.index.strftime('%d/%m/%y'))
+
+        response = jsonify(
+            cobre=cobre,
+            zinco=zinco,
+            aluminio=aluminio,
+            chumbo=chumbo,
+            estanho=estanho,
+            niquel=niquel,
+            dolar=dolar,
+            data=data)
+
+        response.headers.add("Access-Control-Allow-Origin", '*')
+        return response
+
 
     def latest_values():
         parse.uses_netloc.append("postgres")
