dc0a331a64e46cca03a464bad84dd07537161de7
Tiago Cordeiro <tiago@mulhergorila.com>
2018-10-24 15:28:38 -0300

    Adiciona mais testes

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,17 @@
 from flask import url_for
 
 
-def test_status_code_200(client):
+def test_status_code_index(client):
     assert client.get(url_for('index')).status_code == 200
+
+
+def test_status_code_grafico(client):
+    assert client.get(url_for('grafico')).status_code == 200
+
+
+def test_status_code_cotacao(client):
+    assert client.get(url_for('lme_cotacao')).status_code == 200
+
+
+def test_status_code_summary(client):
+    assert client.get(url_for('summary')).status_code == 200
